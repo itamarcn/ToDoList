34f14468f465cc18e370d733a2633ab535df2402
Itamar Cohen <itamarcohen80@gmail.com>
2023-11-17 14:53:05 +0200

    change host url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
-    socketUrl: 'http://localhost:3000'
+    apiUrl: 'https://to-do-list-server-kappa.vercel.app:3000',
+    socketUrl: 'https://to-do-list-server-kappa.vercel.app:3000'
 };
